5773cfa2458ae470e12213cb61634d2154b70bda
Unknown <rtejakusuma21@yahoo.com>
2017-11-30 23:58:47 +0700

    bikin message error nya

--- rm.c
+++ rm.c
@@ -1,3 +1,13 @@
 #include "type.h"
 #include "stat.h"
 #include "user.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+	if(argc<2)
+	{
+		printf(2,"Cara: rm filenya atau rm -r dir\n");
+		exit();
+	}
+}
