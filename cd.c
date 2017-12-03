e376b3d627ba5da3789e379a9c1c2e6d1c3f0d8d
Unknown <rtejakusuma21@yahoo.com>
2017-12-03 10:54:16 +0700

    monggo

--- cd.c
+++ cd.c
@@ -5,9 +5,21 @@
 
 int main (int argc, int *argv[])
 {
+	int coba= chdir(argv[1]);
+
 	if(argc<=1)
 	{
 		printf(2,"Maaf input belum sesuai kali\n");
 		exit();
 	}
+	
+	if(coba==0)
+	{
+		printf(2,"gagal akses %s\n",argv[1]);
+		exit();
+	}
+	else
+		printf(1,"suskses\n");
+
+	exit();
 }
