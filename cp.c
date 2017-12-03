e6a2bf3a6e295be80d8ea07ece6f4888f6fd22de
Unknown <rtejakusuma21@yahoo.com>
2017-12-03 11:26:48 +0700

    qwer

--- cp.c
+++ cp.c
@@ -8,4 +8,15 @@ char buf[512];
 int main (int argc, int * argv[])
 {
 	int fd0,fd1,n;
+	if(argc<=2)
+	{
+		printf(1,"butuh 2 argumen bro\n");
+		exit();
+	}
+
+	if((fd0=open(argv[1],O_RDONLY))<0)
+	{
+		printf(1,"tidak bisa membuka %s\n",argv[1]);
+		exit();
+	}
 }
