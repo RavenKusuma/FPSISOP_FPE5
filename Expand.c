87c33cb6f82cd3c2438e4f6e500af18b6b1309d4
Gilberttherry <gilbertherry333099@gmail.com>
2017-12-08 13:55:27 +0700

    buat expand.c

---
+++ Expand.c
@@ -0,0 +1,20 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int length(char *input){
+    int length = 0;
+    while(input[length]!='\0')  
+    {
+        length++;
+    }
+    return length;
+}
+
+int main(int argc, char *argv[]){
+	int f1;
+	char hasil[1000], buf[1000];
+	
+	exit();
+}
